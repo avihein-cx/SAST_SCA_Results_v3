528497c5c49e660e337f3d5fae4ab5d05c39239a
Avi Hein <avi.hein@checkmarx.com>
2024-03-26 16:23:20 +0200

    after making fix from ai security champion hsts security champion

--- CxSAST-SCA-Results-Frankestain/grotle-master/dashboard/server.js
+++ CxSAST-SCA-Results-Frankestain/grotle-master/dashboard/server.js
@@ -15,9 +15,11 @@ app.use(helmet({
 // Serve only the static files form the dist directory
 app.use(express.static(__dirname + '/dist/torterra-dashboard'));
 
-app.get('*', function(req,res) {
+app.get('*', function(req,res) {// /CxSAST-SCA-Results-Frankestain/grotle-master/dashboard/server.js:18
   res.sendFile(path.join(__dirname + '/dist/torterra-dashboard/index.html'));
-});
+  res.header('Strict-Transport-Security', 'max-age=31536000; includeSubDomains');
+// method continues ...
+
 
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
