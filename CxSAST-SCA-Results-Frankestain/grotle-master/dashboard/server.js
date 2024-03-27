9d1d57737a00966a635beaf3e59a748411ff8bdf
Avi Hein <avi.hein@checkmarx.com>
2024-03-27 11:43:50 +0200

    hsts header

--- CxSAST-SCA-Results-Frankestain/grotle-master/dashboard/server.js
+++ CxSAST-SCA-Results-Frankestain/grotle-master/dashboard/server.js
@@ -1,23 +1,20 @@
 const express = require('express');
-const path = require('path');
-const helmet = require('helmet')
+const helmet = require('helmet');
 
 const app = express();
 
-app.use(helmet({
-  hsts: {
-    maxAge: 31536000,
-    includeSubDomains: true,
-    preload: true
-  }
-}))
+app.use(helmet.hsts({ 
+  maxAge: 31536000,  // Set the `max-age` value to 1 year
+  includeSubDomains: true, // Add the `includeSubDomains`
+  preload: true // Opt-in for HSTS preload list
+}));
 
-// Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/torterra-dashboard'));
-
-app.get('*', function(req,res) {// /CxSAST-SCA-Results-Frankestain/grotle-master/dashboard/server.js:18
+app.get('*', function(req,res) {
   res.sendFile(path.join(__dirname + '/dist/torterra-dashboard/index.html'));
-  res.header('Strict-Transport-Security', 'max-age=31536000; includeSubDomains');
+});
+
+// method continues ...
+
 // method continues ...
 
 
